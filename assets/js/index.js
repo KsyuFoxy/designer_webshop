fde647670e6abc281125f0b351e48e809cd61744
Oksana Kondratiuk <ksyu@web.de>
2017-11-23 17:56:26 +0100

    create responsive navigation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,26 @@
-window.onload = function() {
-
-    // document.write("It works.");
-
-}
+$(window).on('load resize', function mobileViewUpdate() {
+    var viewportWidth = $(window).width();
+    if (viewportWidth < 768) {
+        $(".nav").addClass("nav-stacked");
+        $('.header-content').removeClass('container').addClass('container-fluid');
+        $('.nav li').hide();
+        $('.nav-menu-close').hide();
+        $('.nav-menu-open').show();
+        $('.nav').click(function() {
+            $('.nav li').fadeToggle();
+            $('.nav-menu-close').toggle();
+            $('.nav-menu-open').toggle();
+        });
+        $('.search-icon').click(function() {
+            $('.search-icon').toggleClass('glyphicon-search').toggleClass('glyphicon-remove');
+            $('.search-additional-content').slideToggle();
+        })
+    }
+    // if (viewportWidth < 991) {
+    //
+    // }
+    else {
+        $(".nav").removeClass("nav-stacked");
+        $('.header-content').removeClass('container-fluid').addClass('container');
+    }
+});
